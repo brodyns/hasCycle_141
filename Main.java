39924755291994ec364c09217abd718ae4fb7e83
brodyns <118648344+brodyns@users.noreply.github.com>
2024-02-28 21:22:05 +0800

    141. 环形链表

---
+++ Main.java
@@ -0,0 +1,32 @@
+﻿class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        do{
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }while(slow != fast);
+        return true;
+    }
+}
